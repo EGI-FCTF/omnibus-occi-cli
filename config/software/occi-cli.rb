5acc3f40a707f86b5cd22baea2f15de5317281c8
Boris Parak <parak@cesnet.cz>
2014-03-15 14:14:51 +0100

    Updated install & uninstall scripts

--- config/software/occi-cli.rb
+++ config/software/occi-cli.rb
@@ -6,10 +6,9 @@ dependency "rubygems"
 
 build do
   gem "install occi-cli -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}"
-  command "rm -rf /opt/occi-cli/embedded/docs"
-  command "rm -rf /opt/occi-cli/embedded/share/man"
-  command "rm -rf /opt/occi-cli/embedded/share/doc"
-  command "rm -rf /opt/occi-cli/embedded/ssl/man"
-  command "rm -rf /opt/occi-cli/embedded/info"
+  command "rm -rf #{install_dir}/embedded/docs"
+  command "rm -rf #{install_dir}/embedded/share/man"
+  command "rm -rf #{install_dir}/embedded/share/doc"
+  command "rm -rf #{install_dir}/embedded/ssl/man"
+  command "rm -rf #{install_dir}/embedded/info"
 end
-
